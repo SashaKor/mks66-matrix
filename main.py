6884e30f3195311a5e1f5a8bf0c8b1b358081e2c
SashaKor <akoroza@stuy.edu>
2019-02-17 16:55:58 -0500

    done

--- main.py
+++ main.py
@@ -3,10 +3,64 @@ from draw import *
 from matrix import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
-matrix = new_matrix()
-add_edge(matrix,0,0,1,200,200,1)
-print_matrix(matrix)
+color = [ 40, 0, 100 ]
 
-draw_lines( matrix, screen, color )
+#draws a square given bottom left starting point and size
+def drawSquare(screen,color,x,y,size):
+    matrix = new_matrix(2,2)
+    add_edge(matrix,x,y,0,x+size,y,0)
+    add_edge(matrix,x+size,y,0,x+size,y+size,0)
+    add_edge(matrix,x+size,y+size,0,x,y+size,0)
+    add_edge(matrix,x,y+size,0,x,y,0)
+    draw_lines(matrix, screen, color)
+
+x0=0
+y0=0
+for i in range(70):
+    drawSquare(screen,color,x0,y0,100)
+    x0+=10
+    y0+=10
+    color[0]+=10
+    color[1]+=5
+
+x0=0
+y0=500
+for i in range(70):
+    drawSquare(screen,color,x0,y0,100)
+    x0+=10
+    y0-=10
+    color[0]-=10
+    color[1]+=20
+
+'''
+test section 
+'''
+
+A = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+B = [[11,12,13,14],[15,16,17,18],[19,20,21,22],[23,24,25,26]]
+
+matrix_mult(A,B)
+print("~~~~~~~~~Multiplying A*B~~~~~~~~~~")
+print("printing A")
+print_matrix(A)
+print("printing B")
+print_matrix(B)
+print("~~~~~~~~~Multiplying B*A (b has been altered)~~~~~~~~~~")
+matrix_mult(B,A) #note B has been altered
+print("printing A")
+print_matrix(A)
+print("printing B")
+print_matrix(B)
+
+print("~~~~~~~~~More Cases~~~~~~~~~~")
+Z = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+C = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+ident(Z)
+matrix_mult(Z,C)
+print("testing ident")
+print("printing C")
+print(C)
+print("printing Z (z*c where z is identity)")
+print(Z)
+
 display(screen)
--- matrix.py
+++ matrix.py
@@ -49,9 +49,6 @@ def matrix_mult( m1, m2 ):
             for elem in row: #multiplies row elem with corresponding col elem
                 mult_val= elem*(m2[cnt][i])
                 sum+= mult_val #calculating all the sums
-                #print("sum: " +str(sum))
-                #print("multval: " +str(mult_val))
-                #print("colval: " +str(m2[cnt][i]))
                 cnt+=1
             new_row.append(sum)
         m2_result.append(new_row)
@@ -72,39 +69,3 @@ def new_matrix(rows = 4, cols = 4):
         for r in range( rows ):
             m[c].append( 0 )
     return m
-
-'''
-test section
-'''
-'''
-test1= [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
-test2= new_matrix()
-
-print_matrix(test1)
-print("~~~~~~~~~~~~")
-print_matrix(test2)
-
-ident(test1)
-print_matrix(test1)
-
-ident(test2)
-print_matrix(test2)
-
-A = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-B = [[11,12,13,14],[15,16,17,18],[19,20,21,22],[23,24,25,26]]
-
-matrix_mult(A,B)
-print_matrix(A)
-print_matrix(B)
-matrix_mult(B,A) #note B has been altered
-print_matrix(A)
-print_matrix(B)
-
-test1= [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
-Z = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-C = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-ident(Z)
-matrix_mult(Z,C)
-print(C)
-print(Z)
-'''
